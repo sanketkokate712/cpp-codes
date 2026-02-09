51f904bbbfc74b7738fc8fabee038c33201f19bf
Sanket Kokate <sanketkokate712@gmail.com>
2026-02-09 12:00:00 +0530

    Fixing missed contribution

--- searchinsert.cpp
+++ searchinsert.cpp
@@ -7,10 +7,16 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         int st=0;
+
+
+
         int end=nums.size()-1;
        while(st <= end){
         int mid = st + (end-st)/2;
         if (target > nums[mid] ){
+
+
+            
             st = mid+1;
         }
         else if (target < nums[mid] ){
@@ -22,6 +28,12 @@ public:
         }
         return st;
     }
+
+
+
+
+
+
     
        
        
