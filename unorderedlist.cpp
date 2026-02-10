c6726c070935a863f8fdefa9ff7319d8a0c0938e
Sanket Kokate <sanketkokate712@gmail.com>
2026-02-10 12:00:00 +0530

    Backdated commit for Feb 10

--- unorderedlist.cpp
+++ unorderedlist.cpp
@@ -4,27 +4,15 @@
 using namespace std;
 
 int main() {
-    vector<int> nums = {1, 2, 2, 3, 1, 4, 2};
-
-
-    
+    vector<int> nums = {1, 2, 2, 3, 1, 4, 2}; 
     unordered_map<int, int> freq;
-
-
-
     for (int x : nums) {
         freq[x]++;
     }
-
-
-
     // Print frequencies
     for (auto it : freq) {
         cout << it.first << " -> " << it.second << endl;
     }
-
-
-
     return 0;
 }
 
