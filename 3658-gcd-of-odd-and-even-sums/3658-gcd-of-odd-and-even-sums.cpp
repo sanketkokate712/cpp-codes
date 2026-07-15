3e2a240465a516117704a46235e5e894a8d9acc3
Sanket Kokate <186406115+sanketkokate712@users.noreply.github.com>
2026-07-15 08:55:59 +0530

    Time: 1 ms (21.15%), Space: 8.4 MB (99.4%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int sumo =0,sume=0;
-        for(int i =1 ; i<n*2;i=i+2){
-          sumo = sumo + i;
-        }
-         for(int i =2 ; i<n*2;i= i+2){
-            sume = sume + i;
-        }
-        return gcd(sumo,sume);
+        return n;
     }
 };
+    // int sumo =0,sume=0;
+    //     for(int i =1 ; i<n*2;i=i+2){
+    //       sumo = sumo + i;
+    //     }
+    //      for(int i =2 ; i<n*2;i= i+2){
+    //         sume = sume + i;
+    //     }
+    //     return gcd(sumo,sume);
+    // }
